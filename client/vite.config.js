65a563e80ebf47170dcb0d5729de576cca1bb07b
Hectorv72 <hectorvaldezfsa13@gmail.com>
2022-10-18 16:27:19 -0300

    cliente iniciado desde 0 con vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,19 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      /* '/api': {
-        target : 'http://jsonplaceholder.typicode.com/users',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }, */ 
-      '/back': {
-        target : 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/back/, '')
-      } 
-    }
-  }
+  plugins: [react()]
 })
